083e7233773842651a1b94f9ef237d35a3413163
YoHan Cho <devyhan93@gmail.com>
2022-12-22 09:10:51 +0900

    Wip

--- Package.swift
+++ Package.swift
@@ -3,22 +3,22 @@
 import PackageDescription
 
 let package = Package(
-    name: "APIRouter",
-    platforms: [
-      .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)
-    ],
-    products: [
-        .library(
-            name: "APIRouter",
-            targets: ["APIRouter"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "APIRouter",
-            dependencies: []),
-        .testTarget(
-            name: "APIRouterTests",
-            dependencies: ["APIRouter"]),
-    ]
+  name: "URLRouter",
+  platforms: [
+    .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)
+  ],
+  products: [
+    .library(
+      name: "URLRouter",
+      targets: ["URLRouter"]),
+  ],
+  dependencies: [],
+  targets: [
+    .target(
+      name: "URLRouter",
+      dependencies: []),
+    .testTarget(
+      name: "URLRouterTests",
+      dependencies: ["URLRouter"]),
+  ]
 )
